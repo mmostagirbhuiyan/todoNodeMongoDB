4da60d630180badcd157ccc4ec697b4bdcee216b
mmostagirbhuiyan <mmostagirbhuiyan@gmail.com>
2020-04-23 10:44:52 -0400

    Fixed heroku port error

--- app.js
+++ app.js
@@ -143,6 +143,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(process.env.PORT, function() {
   console.log("Server started on port 3000");
 });
